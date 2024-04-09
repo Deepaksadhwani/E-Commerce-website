28de08c54214d750374d0cfae9328c5ea7f854f6
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-04-09 22:23:05 +0530

    added cards into Store component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Card = ({ title, url, price }) => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="mx-auto flex flex-col items-center justify-center space-y-1 rounded-md  bg-white p-4 transition-all duration-200 hover:scale-[1.08] ">
       <img src={url} alt="image-logo" />
-      <h3>{title}</h3>
-      <div>
+      <h3 className="border-b-1 jus flex border-gray-700">{title}</h3>
+      <div className="flex w-full items-center justify-between p-2 font-semibold ">
         <p>💲{price}</p>
-        <button></button>
+        <button className="rounded-md bg-blue-500 p-2 text-white transition-all duration-100  hover:bg-blue-600">
+          ADD TO CART
+        </button>
       </div>
     </div>
   );
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -4,9 +4,11 @@ import Card from "../components/Card";
 
 const Store = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className=" p-4 font-serif text-4xl">Music</h1>
-      <div className=" grid  grid-cols-2 gap-10 space-x-20">
+    <div className="flex flex-col items-center justify-center bg-gray-400">
+      <h1 className=" p-4 font-serif  text-4xl font-semibold text-gray-700">
+        Music
+      </h1>
+      <div className="  mt-10 grid grid-cols-2 gap-x-40 gap-y-20">
         {productsArr.map((item) => (
           <Card url={item.imageUrl} price={item.price} title={item.title} />
         ))}
