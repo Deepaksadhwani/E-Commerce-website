1a0a01a42d07ab7b1379a1591cbc4078c86fef92
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-04-11 14:39:09 +0530

    Replace Link with NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,40 @@
 import React, { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { CartContext } from "../contexts/CartContextProvider";
 import { TiShoppingCart } from "react-icons/ti";
 
 const NavBar = () => {
   const [shouldCartVisible, setShouldCartVisible] = useState(false);
   const { cartDataArray } = useContext(CartContext);
+  const activeClass = "text-yellow-300"
+
   return (
-    <div className=" relative flex items-center justify-center border-b-4 border-red-700 bg-black p-4">
+    <div className="relative flex items-center justify-center border-b-4 border-red-700 bg-black p-4">
       <div className="flex w-3/12 justify-evenly text-xl font-medium text-white">
-        <Link to="/">
-          <button onClick={() => setShouldCartVisible(false)}>Home</button>
-        </Link>
-        <Link to="/store">
-          <button onClick={() => setShouldCartVisible(true)}>Store</button>
-        </Link>
-        <Link to="/about">
-          <button onClick={() => setShouldCartVisible(false)}>About</button>
-        </Link>
+        <NavLink to="/" onClick={() => setShouldCartVisible(false)} className={(({isActive}) => isActive ? activeClass : undefined)}>
+          Home
+        </NavLink>
+        <NavLink to="/store" onClick={() => setShouldCartVisible(true)} className={(({isActive}) => isActive ? activeClass :undefined)}>
+          Store
+        </NavLink>
+        <NavLink
+          to="/about"
+          onClick={() => setShouldCartVisible(false)}
+          className={(({isActive}) => isActive ? activeClass : undefined)}
+        >
+          About
+        </NavLink>
       </div>
       {shouldCartVisible && (
-        <Link to="/cart">
-          <button className="absolute  right-16 top-4 flex items-center gap-x-2 text-xl font-medium text-white">
-            <TiShoppingCart/>
-            Cart <span className="text-md  font-semibold text-yellow-300 shadow-lg shadow-orange-50">{cartDataArray.length}</span>
+        <NavLink to="/cart">
+          <button className="absolute right-16 top-4 flex items-center gap-x-2 text-xl font-medium text-white">
+            <TiShoppingCart />
+            Cart
+            <span className="text-md font-semibold text-yellow-300 shadow-lg shadow-orange-50">
+              {cartDataArray.length}
+            </span>
           </button>
-        </Link>
+        </NavLink>
       )}
     </div>
   );
