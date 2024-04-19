a9b3331cec44c8fa03afc50caf2b923ea98b78de
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-04-19 14:20:13 +0530

    add key in cards which are in store page

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -8,9 +8,8 @@ const Store = () => {
     <div className="mb-[-24px] flex flex-col items-center justify-center bg-blue-300 pb-40 ">
       <div className="  mt-10 grid grid-cols-4 gap-x-10 gap-y-10">
         {productsArr.map((item, index) => (
-          <Link to={`/store/${index}`}>
+          <Link to={`/store/${index}`} key={index}>
             <Card
-              key={index}
               url={item.imageUrl}
               price={item.price}
               title={item.title}
